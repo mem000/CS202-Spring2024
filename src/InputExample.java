d7bfc95be3f9a49b2d363a7684f6604d39fdaee4
mem000 <mahmoudesmatm@gmail.com>
2024-02-11 22:42:24 +0200

    updates . . .

--- src/InputExample.java
+++ src/InputExample.java
@@ -1,2 +1,11 @@
 public class InputExample {
+    public static void main(String[ ] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your age in years: ");
+        double age = input.nextDouble();
+        System.out.print("Enter your maximum heart rate: ");
+        double rate = input.nextDouble();
+        double fb = (rate - age) * 0.65;
+        System.out.println("Your ideal fat-burning heart rate is " + fb);
+    }
 }
